01a6a59ffc77184e1342212ef534af352215b426
John <johnclarkthiel@Johns-MBP.home>
2016-04-28 13:23:02 -0400

    giving web data its own controller, adding method for querying db to find cities

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,6 +4,20 @@ app.controller('CityController', ['$http', '$scope', '$location', '$rootScope', 
 	this.sendCity = function() {
 		console.log("DOING STUFF");
 		console.log(this.city);
-	}
+		
+		$http.post("/cities/city_search", { city_name: this.city }).then(function(response){
+			console.log(response);
+		}, function(err){
+			console.log(err);
+		});
+
+		// $http.get("/update/").then(function(response){
+		// 	console.log(response);
+		// }, function(err){
+		// 	console.log(err);
+		// });
+	
+		this.city = undefined;
+	};
 
 }]);
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   root 'cities#index'
+
+  get "/update/" => "updates#update_cities"
+
+  resource :cities
+
+  post "/cities/city_search" => "cities#city_search"
 end
+
+#             Prefix Verb   URI Pattern                   Controller#Action
+#               root GET    /                             cities#index
+#             update GET    /update(.:format)             updates#update_cities
+#             cities POST   /cities(.:format)             cities#create
+#         new_cities GET    /cities/new(.:format)         cities#new
+#        edit_cities GET    /cities/edit(.:format)        cities#edit
+#                    GET    /cities(.:format)             cities#show
+#                    PATCH  /cities(.:format)             cities#update
+#                    PUT    /cities(.:format)             cities#update
+#                    DELETE /cities(.:format)             cities#destroy
+# cities_city_search POST   /cities/city_search(.:format) cities#city_search
